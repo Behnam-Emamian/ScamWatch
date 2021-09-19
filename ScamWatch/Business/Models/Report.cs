f8e4bf8ffb5eeeb566c34e06d099d9cbc55a3589
Ben Emamian <ben@ictace.com>
2021-09-19 22:26:24 +1000

    move business and business.tests

---
+++ ScamWatch/Business/Models/Report.cs
@@ -0,0 +1,21 @@
+﻿using Business.Enums;
+using System;
+
+namespace Business.Models;
+
+public class Report
+{
+    public ScamType ScamType { get; set; }
+    public DeliveryMethod DeliveryMethod { get; set; }
+    public DateOnly FirstContactDate { get; set; }
+    public string? Description { get; set; }
+
+    public string ScammerPhoneNumber { get; set; }
+
+    public string TargetFirstName { get; set; }
+    public string TargetLastName { get; set; }
+    public string TargetEmail { get; set; }
+    public string TargetPhoneNumber { get; set; }
+    public Gender TargetGender { get; set; }
+    public State TargetState { get; set; }
+}
