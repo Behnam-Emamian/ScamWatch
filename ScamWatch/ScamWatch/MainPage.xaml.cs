4e6728671193f780453120d33a087ecd3a93fef2
Samira Maleki <samira.maleki1112@gmail.com>
2021-10-01 17:56:46 +0330

    add securestorage

--- ScamWatch/ScamWatch/MainPage.xaml.cs
+++ ScamWatch/ScamWatch/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Services;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
 using System;
 
 namespace ScamWatch
@@ -15,9 +16,21 @@ namespace ScamWatch
             InitializeComponent();
         }
 
+        protected override async void OnAppearing()
+        {
+            this.firstName.Text = await SecureStorage.GetAsync("FirstName");
+            this.lastName.Text = await SecureStorage.GetAsync("LastName");
+            this.email.Text = await SecureStorage.GetAsync("Email");
+            this.phoneNumber.Text = await SecureStorage.GetAsync("PhoneNumber"); ;
+        }
         private void OnSubmitReportClicked(object sender, EventArgs e)
         {
-
+            SecureStorage.SetAsync("FirstName", this.firstName.Text);
+            SecureStorage.SetAsync("lastName", this.lastName.Text);
+            SecureStorage.SetAsync("Email", this.email.Text);
+            SecureStorage.SetAsync("PhoneNumber", this.phoneNumber.Text);
+            SecureStorage.SetAsync("Gender", this.gender.Title);
+            SecureStorage.SetAsync("State", this.state.Title);
         }
     }
 }
